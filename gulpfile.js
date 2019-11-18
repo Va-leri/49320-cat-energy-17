1b3de2bd1d854461e518fbee268b9ef794b9040c
Va-leri <v.budilova@mail.ru>
2019-11-18 13:26:08 +0300

    Добавлена ветка для gh-pages

--- gulpfile.js
+++ gulpfile.js
@@ -17,6 +17,13 @@ var include = require("posthtml-include");
 var del = require("del");
 var htmlmin = require("gulp-htmlmin");
 var jsmin = require("gulp-uglify");
+var gulp = require('gulp');
+var ghPages = require('gulp-gh-pages');
+
+gulp.task('deploy', function () {
+  return gulp.src('./dist/**/*')
+    .pipe(ghPages());
+});
 
 gulp.task("css", function () {
   return gulp.src("source/sass/style.scss")
@@ -81,8 +88,8 @@ gulp.task("copy", function () {
     "source/img/**",
     "source/js/**"
   ], {
-      base: "source"
-    })
+    base: "source"
+  })
     .pipe(gulp.dest("build"));
 });
 
